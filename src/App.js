7d8af4ed49e1497d3951476712025351bf0e3993
Jitesh <kanyaljitesh)@gmail.com>
2022-03-14 19:01:24 +0530

    Enhaced dark-Mode

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
-import './App.css';
-import About from './components/About';
-import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import React, { useState } from "react";
 function App() {
+  const [mode, setmode] = useState("light");
+  const [NavText, setNavText] = useState('Enable Dark Mode')
+  const toggleMode = () => {
+    if (mode === "light") {
+      setmode("dark");
+      setNavText('Disable Dark Mode')
+      document.body.style.backgroundColor = '#042743'
+    } else {
+      setmode("light");
+      setNavText('Enable Dark Mode')
+      document.body.style.backgroundColor = 'White'
+
+
+    }
+
+  };
   return (
-<>
-<Navbar title="TextUtile"   aboutText="About TextUtils"/>
-<div className="container my-3">
-   <TextForm heading="Enter the text to Analyze below"/>
-<About/>
-</div>
-</>  );
+    <>
+      <Navbar
+        title="TextUtile"
+        aboutText="About TextUtils"
+        mode={mode}
+        toggleMode={toggleMode}
+        NavText={NavText}
+      />
+      <div className="container my-3">
+        <TextForm heading="Enter the text to Analyze below" mode={mode} />
+        {/* <About/> */}
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -25,13 +25,14 @@ export default function TextForm(props) {
 
   return (
     <>
-      <div className="container">
+      <div className="container" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
         <h1>{props.heading}</h1>
 
         <div className="mb-3">
           <textarea
             className="form-control"
             value={text}
+            style={{ backgroundColor: props.mode === 'dark' ? '#696969' : 'white', color: props.mode === 'dark' ? 'white' : 'black' }}
             onChange={handleUpChange}
             id="textForm"
             rows="8"
@@ -50,14 +51,14 @@ export default function TextForm(props) {
           Remove extra Spaces
         </button>
       </div>
-      <div className="container my-3">
+      <div className="container my-3" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
         <h2>Your Text Summary</h2>
         <p>
           {text.split(" ").length} Words and {text.length} Characters
         </p>
         <p>{0.08 * text.split(" ").length} Min to read.</p>
         <h2>Preview</h2>
-        <p>{text}</p>
+        <p>{text.length > 0 ? text : 'Enter Something In The Above Text-box To Prewiew It here..'}</p>
       </div>
     </>
   );
